55acc87cbe0975ac9b85d895b5af8c7353de7488
Viacheslav Shcherba <viacheslav.shcherba@vrpconsulting.com>
2020-01-20 14:04:54 +0300

    Fixed mistakes

--- src/aura/ReportsView/ReportsViewController.js
+++ src/aura/ReportsView/ReportsViewController.js
@@ -2,11 +2,15 @@
  * Created by vshcherba on 1/14/2020.
  */
 ({
-    doInit : function (component, event, helper) {
+    doInit: function (component, event, helper) {
         helper.getButtons(component);
     },
 
-    clickOnButton : function (component, event, helper) {
+    clickOnButton: function (component, event, helper) {
         helper.redirectToReport(component, event);
+    },
+
+    getIconName: function (component, event, helper) {
+        return "standard:report";
     }
 })
--- src/aura/ReportsView/ReportsViewHelper.js
+++ src/aura/ReportsView/ReportsViewHelper.js
@@ -2,21 +2,37 @@
  * Created by vshcherba on 1/14/2020.
  */
 ({
-    getButtons : function (component) {
+    getButtons: function (component) {
         let action = component.get("c.getReports");
         action.setCallback(this, function (response) {
             let state = response.getState();
             if (state === "SUCCESS") {
                 let reports = JSON.parse(response.getReturnValue());
                 component.set("v.reports", reports);
+            } else {
+                let toastEvent = $A.get("e.force:showToast");
+                toastEvent.setParams({
+                    "title": $A.get("$Label.c.error"),
+                    "message": $A.get("$Label.c.serverProblems")
+                });
+                toastEvent.fire();
             }
         });
         $A.enqueueAction(action);
     },
 
-    redirectToReport : function (component, event) {
+    redirectToReport: function (component, event) {
+        let navService = component.find("navService");
         let ind = event.getSource().get("v.name");
         let report = component.get("v.reports")[ind];
-        window.open("/" + report.id);
+        let pageReference = {
+            type: 'standard__recordPage',
+            attributes: {
+                recordId: report.id,
+                objectApiName: 'Report',
+                actionName: 'view'
+            }
+        };
+        navService.navigate(pageReference);
     }
 })
